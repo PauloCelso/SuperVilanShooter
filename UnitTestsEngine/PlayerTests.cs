194356566f4a3bdfd1ec5d557ad779752f5e2e76
Pauloc <pc@gmail.com>
2016-01-12 23:26:14 +0000

    Completed the SuperVilanShoorterGame

---
+++ UnitTestsEngine/PlayerTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+
+namespace UnitTestsEngine
+{
+    [TestClass]
+    public class PlayerTests
+    {
+        [TestMethod]
+        public void Should_Create_A_Player
+        {
+
+
+        //    //Assert
+        //    Assert.Th(new Player(){CurrentHitPoints = 1,
+        //    MaximumHitPoints = 2, 
+        //    Gold = 100, 
+        //    ExperiencePoints = 1000,
+        //    Level = 3       
+        //}, GetType(Player));
+        }
+    }
+}
